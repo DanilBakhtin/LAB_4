50fa6e88ab0511f5037b527ff046326c04ae4d1f
DanilBakhtin <72213151+DanilBakhtin@users.noreply.github.com>
2021-10-02 01:54:19 +0800

    Update Config.java

--- Config.java
+++ Config.java
@@ -4,6 +4,6 @@ public class Config {
     protected String dbHost = "localhost";
     protected String dbPort = "3306";
     protected String dbUser = "root";
-    protected String dbPass = "Danil2502";
+    protected String dbPass = "";
     protected String dbName = "Classes";
 }
